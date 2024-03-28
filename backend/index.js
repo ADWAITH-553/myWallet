ce1b051429d91042a158b0ebba37d1837f49d833
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-03-28 19:17:18 +0530

    zod signup schema created

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const bodyParser = require('body-parser');
-const router = require("./routes");
+const router = require("./routes/index");
 const app=express();
 const cors = require('cors');
 app.use(cors());
 app.use(express.json())
-app.use("api/v1",router)
-app.listen(8005,()=>{
+app.use(bodyParser.json());
+app.use('/api/v1',router)
+app.listen(3050,()=>{
     console.log("listening");
 })
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,32 @@
 const express=require('express')
-
+const zod=require('zod');
+const User = require('../db');
+const jwt=require("jsonwebtoken")
+const JWT_SECRET = require('../config');
 const router=express.Router();
-
-router.post("/signup")
+const signUpSchema=zod.object({
+    username:zod.string(),
+    password:zod.string(),
+    firstName:zod.string(),
+    lastName:zod.string()
+})
+router.post("/signup",async (req,res)=>{
+    const body=req.body
+    console.log("hii")
+    const {success}=signUpSchema.safeParse(req.body);
+    if(!success){
+        return res.json({message:"email alredy exists/invalid inputs"})
+    }
+    const user=User.findOne({username:body.username})
+    if(user._id){
+        return res.json({message:"email alredy exists/invalid inputs"})
+    }
+    const dbuser=await User.create(body);
+    const token=jwt.sign({userId:dbuser._id},JWT_SECRET)
+    res.json({
+        message:"user created successfully",
+        token:token
+    })
+})
 
 module.exports = router
