9ce4c27d3fbca9e69ac281c2e3724c08f4f35a2d
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-03-28 17:16:03 +0530

    userroute

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,6 @@
-const expreess=require('express')
-
+const express=require('express')
+const userRouter = require('./user')
 const router=express.Router();
+
+router.use("/user",userRouter)
 module.exports = router;
