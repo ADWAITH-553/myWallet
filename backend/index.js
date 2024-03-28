895ceac7fd98082744d33744ff390af65dba6155
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-03-28 17:31:33 +0530

    secret

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const router = require("./routes");
 const app=express();
 const cors = require('cors');
 app.use(cors());
-
+app.use(express.json())
 app.use("api/v1",router)
 app.listen(8005,()=>{
     console.log("listening");
