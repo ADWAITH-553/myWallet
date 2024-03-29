0ed79693cdae76cc9951c6b6c88703362150ff12
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-03-29 16:27:51 +0530

    signin backened

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -28,5 +28,27 @@ router.post("/signup",async (req,res)=>{
         token:token
     })
 })
+const signinBody = zod.object({
+    username : zod.string(),
+    password : zod.string()
+})
+router.post("/signin",async (req,res)=>{
+    const {success} = signinBody.safeParse(req.body)
+    console.log(req.body)
+    console.log(success)
+    if(!success){
+        return res.status(411).json({
+            message:"error"
+        })
+    }
+
+        const user=await User.findOne({username:req.body.username,password:req.body.password})
+        if(user){
+            const token=jwt.sign({userId:user._id},JWT_SECRET)
+            res.status(200).json({token:token})
+            return
+        }
+        res,status(411).json({message:"error while login"})
+})
 
 module.exports = router
