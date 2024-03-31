3de6cb2ffb46426f7bec3cd92856985a52dd0cd5
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-03-31 11:18:58 +0530

    added backend for searching feature

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -71,4 +71,26 @@ router.put("/",authMiddleware,async(req,res)=>{
     })
 })
 
+router.get("/search",async(req,res)=>{
+    const filter=req.query.filter || "";
+    const users=await User.find({
+        $or:[{
+            firstName:{
+                "$regex":filter
+            },
+            lastName:{
+                "$regex":filter
+            }
+        }]
+    })
+    res.json({
+        user:users.map(user=>({
+            username:user.username,
+            firstName:user.firstName,
+            lastName:user.lastName,
+            _id:user._id
+        }))
+
+    })
+})
 module.exports = router
