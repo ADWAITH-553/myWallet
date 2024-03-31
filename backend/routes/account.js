57daab3f566c1f3f2287bea19736258ccc8e046f
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-03-31 17:38:41 +0530

    to get balance

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,11 +1,18 @@
 const express=require('express')
 
-const authMiddleware=require('../middlwware')
+const {authMiddleware}=require('../middlwware')
 const {Account}=require('../db')
 const mongoose=require('mongoose')
 const router=express.Router()
 
-
+router.get("/balance",authMiddleware,async(req,res)=>{
+    const account=await Account.findOne({
+        userId:req.userId
+    })
+    res.json({
+        balance:account.balance
+    })
+})
 
 
 
