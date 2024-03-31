23bd59d4ef0e116eca3c13152f4c88f9884b1c4b
ADWAITH-553 <adwaithshinod553@gmail.com>
2024-03-31 17:35:19 +0530

    account router created

---
+++ backend/routes/account.js
@@ -0,0 +1,16 @@
+const express=require('express')
+
+const authMiddleware=require('../middlwware')
+const {Account}=require('../db')
+const mongoose=require('mongoose')
+const router=express.Router()
+
+
+
+
+
+
+
+
+
+module.exports=router;
